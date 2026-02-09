c31a516a7de3c7cbfa81b174eb84d02f5ba56526
Pradip Rathod <pradiprp062@gmail.com>
2026-02-09 17:39:23 +0530

    Initial commit: Portfolio website

---
+++ node.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+window.addEventListener("scroll", () => {
+let current = "";
+
+sections.forEach(section => {
+const sectionTop = section.offsetTop - 120;
+if(scrollY >= sectionTop){
+current = section.getAttribute("id");
+}
+});
+
+navLinks.forEach(a => {
+a.classList.remove("active");
+if(a.getAttribute("href") === "#" + current){
+a.classList.add("active");
+}
+});
+});
